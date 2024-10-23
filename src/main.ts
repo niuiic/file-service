b304663c5da18235cbce03cc0469d75c8f5829dd
niuiic <niuiicde@gmail.com>
2024-10-23 16:43:17 +0800

    style: format

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { FastifyAdapter } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  )
   await app.listen(3000, '0.0.0.0')
 }
 bootstrap()
