185a4cc76dfe0fa9e7deafa92b0edf5fdd64d03c
niuiic <niuiicde@gmail.com>
2024-12-05 15:16:55 +0800

    chore: fix drizzle config

--- app/drizzle.config.ts
+++ app/drizzle.config.ts
@@ -1,14 +1,16 @@
-import './src/env'
 import { defineConfig } from 'drizzle-kit'
 import { readFileSync } from 'fs'
 import { join } from 'path'
 
+const config = JSON.parse(
+  readFileSync(join(process.cwd(), 'config.json')).toString()
+)
+
 export default defineConfig({
-  dialect: 'postgresql',
+  dialect: config.db.type,
   schema: join(process.cwd(), 'src/modules/db/schema.ts'),
   dbCredentials: {
-    url: JSON.parse(readFileSync(join(process.cwd(), 'config.json')).toString())
-      .DB_URL
+    url: `${config.db.type}://${config.db.username}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.database}`
   },
   casing: 'snake_case'
 })
