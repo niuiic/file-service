6b86b2e20963e8fd65e5b294349f36e4bbac288b
niuiic <niuiicde@gmail.com>
2025-01-02 16:42:47 +0800

    test: upload with stream

--- app/src/modules/file/controller/upload.test.ts
+++ app/src/modules/file/controller/upload.test.ts
@@ -1,10 +1,11 @@
 import type { NestFastifyApplication } from '@nestjs/platform-fastify'
-import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, test } from 'vitest'
 import request from 'supertest'
 import type { RawServerDefault } from 'fastify'
 import { createHash } from 'crypto'
-import type { FileInfo } from './fileInfo'
 import { initTestApp } from '@/share/test'
+import type { Writable } from 'stream'
+import { Readable } from 'stream'
 
 describe('file upload controller', () => {
   let app: NestFastifyApplication<RawServerDefault>
@@ -15,43 +16,29 @@ describe('file upload controller', () => {
 
   // %% uploadFileByStream %%
   test('uploadFileByStream', async () => {
-    const text = new Date().toString().repeat(100)
-    const fileData = Buffer.from(text)
-    const fileHash = createHash('md5').update(text).digest('hex')
+    const fileData = new Date().toString().repeat(100)
+    const fileHash = createHash('md5').update(fileData).digest('hex')
 
-    let fileInfo: FileInfo
+    const { promise, resolve } = Promise.withResolvers()
+    setTimeout(() => resolve(undefined), 4000)
+    const readable = new Readable({
+      read() {
+        this.push(fileData)
+        this.push(null)
+      }
+    })
 
-    const uploadFile = () =>
+    readable.pipe(
       request(app.getHttpServer())
         .post('/file/upload/stream')
         .set('Content-Type', 'application/octet-stream')
         .query({
           fileHash,
           fileName: 'test.txt'
-        })
-        .send(fileData)
-        .then((x) => {
-          fileInfo = x.body
-          expect((fileInfo.name = 'test.txt'))
-        })
+        }) as unknown as Writable
+    )
 
-    const uploadSameFile = () =>
-      request(app.getHttpServer())
-        .post('/file/upload/stream')
-        .query({
-          fileHash,
-          fileName: 'test2.txt'
-        })
-        .send(fileData)
-        .then((x) => {
-          const newFileInfo: FileInfo = x.body
-          expect(newFileInfo.uploadTime).toBe(fileInfo.uploadTime)
-          expect(newFileInfo.relativePath).toBe(fileInfo.relativePath)
-          expect(newFileInfo.name).not.toBe(fileInfo.name)
-        })
-
-    await uploadFile()
-    await uploadSameFile()
+    return promise
   })
 
   // %% uploadFileByHash %%
